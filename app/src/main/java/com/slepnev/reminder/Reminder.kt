8137d4245b2779b77b7fa83520e34bf3c8d9156b
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-01 20:01:47 +0300

    Initial commit, copied the level4-example result

---
+++ app/src/main/java/com/slepnev/reminder/Reminder.kt
@@ -0,0 +1,20 @@
+package com.slepnev.reminder
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "reminderTable")
+data class Reminder(
+    @ColumnInfo(name = "reminder")
+    var reminderText: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+
+
+) : Parcelable
---
+++ app/src/main/java/com/slepnev/reminder/ReminderRepository.kt
@@ -0,0 +1,29 @@
+package com.slepnev.reminder
+
+import android.content.Context
+
+public class ReminderRepository(context: Context) {
+    private var reminderDao: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDao = reminderRoomDatabase!!.reminderDao()
+    }
+
+    suspend fun getAllReminders(): List<Reminder> {
+        return reminderDao.getAllReminders()
+    }
+
+    suspend fun insertReminder(reminder: Reminder) {
+        return reminderDao.insertReminder(reminder)
+    }
+
+    suspend fun deleteReminder(reminder: Reminder) {
+        return reminderDao.deleteReminder(reminder)
+    }
+
+    suspend fun updateReminder(reminder: Reminder) {
+        return reminderDao.updateReminder(reminder)
+    }
+}
+
